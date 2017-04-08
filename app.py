832a28452805fcac972686102dceb32b022ad674
jkthaha <jkthaha@gmail.com>
2017-04-08 22:28:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,76 +16,26 @@ app = Flask(__name__)
 
 
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['GET'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-    
     res = processRequest(req)
-
-    res = json.dumps(res, indent=4)
-    
+    res = json.dumps(res, indent=4)    
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
 
 def processRequest(req):
-
     if req.get("result").get("action") != "wiki":        
         return {}
-    #baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    #yql_query = makeYqlQuery(req)
-    #if yql_query is None:
-    #    return {}
-    #yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
-    #result = urllib.urlopen(yql_url).read()
-    #data = json.loads(result)
+    fin = wikipedia.s
     res = makeWebhookResult()
     return res
 
 
-#def makeYqlQuery(req):
-#    result = req.get("result")
-#    parameters = result.get("parameters")
-#    global fl
-#    city = parameters.get("par1")
-##    if city is None:
-#        return None
-
-#    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
-
 def makeWebhookResult():
-    #query = data.get('query')
-    #if query is None:
-    #    return {}
-
-    #result = query.get('results')
-    
-    #if result is None:
-    #    return {}
-
-    #channel = result.get('channel')
-    #if channel is None:
-    #    return {}
-
-    #item = channel.get('item')
-    
-    #location = channel.get('location')
-    
-    #units = channel.get('units')
-    #if (location is None) or (item is None) or (units is None):
-    #    return {}
-
-    #condition = item.get('condition')
-    #if condition is None:
-    #    return {}
-
-    # print(json.dumps(item, indent=4))
-
     speech = "good night"
-   
-
     return {
         "speech": speech,
         "displayText": speech,
