9b4ca89ba4130625ea72f36816c6947b79f24cda
jkthaha <jkthaha@gmail.com>
2017-04-09 12:25:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def processRequest(req):
         
         if oper1 == "+":
             res1 = int(str1) + int(str2)
-            tom = "The result is "+ str(res1)
+            tom = "The result is "
             res = makeWebhookResult(tom)
             return res
         elif oper1 == "-":
