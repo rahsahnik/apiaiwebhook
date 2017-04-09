71e93b26e0765a6247877fa059c9b156f12cda96
jkthaha <jkthaha@gmail.com>
2017-04-09 13:21:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def processRequest(req):
         baseurl = "https://query.yahooapis.com/v1/public/yql?"
         yql_query = makeYqlQuery(req)
         if yql_query is None:
-        return {}
+            return {}
         yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
         result = urllib.urlopen(yql_url).read()
         data = json.loads(result)
